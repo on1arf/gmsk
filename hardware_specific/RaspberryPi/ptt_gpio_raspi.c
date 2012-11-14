be687dd209e43a012ed4cbcdaf8d7e52acfd505e
Kristoff Bonne <github@krbonne.net>
2012-11-14 23:01:44 +0100

    ptt_gpio_raspi and audiocap_pcm_gpio_raspi

--- hardware_specific/RaspberryPi/ptt_gpio_raspi.c
+++ hardware_specific/RaspberryPi/ptt_gpio_raspi.c
@@ -1,4 +1,18 @@
+// PTT_GPIO_RASPI
+
+// PTT driver for GMSK modem.
+// Sets GPIO port based on the presents of a posix filelock on file
+// Raspberry pi version using bcm2835 library
+
+// This application forks a seperate process that runs as "root"
+// to do GPIO switching
+
+// Known problems:
+// If the main application unexpectably dies, the child process (that is
+// reading input from the parent process) does not die
+
 #include <errno.h>
+#include <string.h>
 #include <stdio.h>
 
 #define _GNU_SOURCE
@@ -12,11 +26,13 @@
 
 // posix threads + inter-process control
 #include <pthread.h>
-//#include <signal.h>
 
 #include <sys/stat.h>
 #include <fcntl.h>
 
+// needed for getppid
+#include <sys/types.h>
+#include <unistd.h>
 
 #define FOREVER 1
 
@@ -32,6 +48,7 @@
 // global data structure
 typedef struct {
 	int out;
+	int readpipe;
 	int writepipe;
 } globaldatastr;
 
@@ -41,8 +58,9 @@ globaldatastr global;
 
 // defined below
 void * funct_writetochild (void * globalin);
+void * do_child (globaldatastr * p_global);
 
-main (int argc, char ** argv) {
+int main (int argc, char ** argv) {
 int pipefd[2];
 int ret;
 pid_t pid;
@@ -54,6 +72,9 @@ char * lockfile;
 
 uid_t myuid, myeuid; // uid and effective uid
 gid_t mygid, myegid; // gid and effective gid
+
+// vars for endless loop in main application
+int lret, laststate, newstate;
 
 
 pthread_t thr_writetochild;
@@ -94,7 +115,7 @@ if (myeuid != 0) {
 	exit(-1);
 }; // end if
 
-if (myeuid != 0) {
+if (myegid != 0) {
 	fprintf(stderr,"Error: EGID not ROOT: GPIO needs root priviledges. Please use setuid method: chown root:root %s ; chmod 6755 %s\n",argv[0],argv[0]);
 	exit(-1);
 }; // end if
@@ -122,83 +143,17 @@ if (ret < 0) {
 }; // end if
 
 // copy write pipe to global data, so it can be used by subthread
+global.readpipe=pipefd[0];
 global.writepipe=pipefd[1];
 
 
 // create and start child process
 pid=fork();
 
-if (pid) {
-	// parent process
-
-	// local vars
-	int lret, laststate, newstate;
-
-	// init local vars
-	laststate=-1;
-	
-	// drop priviledge level
-	ret=setuid(myuid);
-	ret=setgid(mygid);
-
-	if (ret < 0) {
-		fprintf(stderr,"Error: setuid failed: %s\n",strerror(errno));
-		exit(-1);
-	}; // end if
-
-
-	// close unused read pipe (not used in parent process)
-	close (pipefd[0]);
-
-	// create subthread.
-	// this thread will receive input from the child process and
-	// pass that on to the main thread
-	pthread_create (&thr_writetochild, NULL, funct_writetochild, (void *) &global);
-
-
-	// main application: check lock-file and write status to global data
-	// create lock-file
-	lockfd=open(lockfile, O_WRONLY| O_CREAT , S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
-	if (lockfd < 0) {
-		fprintf(stderr,"Error: lockfile %s could not be opened or created. Error %d(%s)\n",lockfile,errno,strerror(errno));
-		exit(-1);
-	}; // end if
-
-	while (FOREVER) {
-
-		lret = lockf(lockfd, F_TEST, 0);
-
-		if (lret < 0) {
-			// file is locked,
-			global.out = 1;
-			newstate = 1;
-		} else {
-			// file is not locked
-			global.out = 0;
-			newstate = 0;
-		}; // end else - if
-
-		if (laststate != newstate) {
-			// wait for 1 second
-			printf("last state was %d, now %d\n",laststate,newstate);
-			laststate=newstate;
-			sleep(1);
-		}; // end if		
-
-		// wait 50 ms
-		usleep(50000);
-
-	}; // end endless loop while
-
-
-} else {
-
+if (!pid) {
 	// child-process
 	// should run as root
 	// does actual GPIO
-
-	// local vars
-	int ret;
 
 	if (!bcm2835_init()) {
 		fprintf(stderr,"Error: could not init BCM2835! \n");
@@ -208,48 +163,84 @@ if (pid) {
 	// set the pin to be output
 	bcm2835_gpio_fsel(PIN, BCM2835_GPIO_FSEL_OUTP);
 
-	
-	// endless loop
 	while (FOREVER) {
-		char c;
+		do_child(&global);
+	}; // end while
 
-		
-		// read data from PIPE
-		ret=read(pipefd[0],&c,1);
-
-		if (ret) {
-			if (c == 0x30) {
-				// Turn it off
-				bcm2835_gpio_write(PIN, LOW);
-
-			} else if (c == 0x31) {
-				// Turn it on
-				bcm2835_gpio_write(PIN, HIGH);
-
-			} else {
-				// error: unknown char
-				fprintf(stderr,"Warning: Child received unknown char from parent: %02X\n",c);
-			}; // end else - elsif - if
-		
-			// sleep 50 ms
-			usleep(50000);
-		} else {
-			// error in read
-			fprintf(stderr,"Error: Child did not receive any data from parent\n");
-
-			// sleep 1 second
-			sleep(1);
-		};  // read successfull
-
-
-	}; // end while (endless loop)
-}; // end else - if
-
-
-}; // end main
+	fprintf(stderr,"Error: child process dropped out of endless loop: should not happen!\n");
+	exit(-1);
+	
+}; // end if (child process)
 
 
 
+// main application (parent process)
+
+// drop priviledge level
+ret=setuid(myuid);
+ret=setgid(mygid);
+
+if (ret < 0) {
+	fprintf(stderr,"Error: setuid failed: %s\n",strerror(errno));
+	exit(-1);
+}; // end if
+
+
+	
+// close unused read pipe (not used in parent process)
+close (global.readpipe);
+
+// create subthread.
+// this thread will receive input from the child process and
+// pass that on to the main thread
+pthread_create (&thr_writetochild, NULL, funct_writetochild, (void *) &global);
+
+
+// main application: check lock-file and write status to global data
+// create lock-file
+lockfd=open(lockfile, O_WRONLY| O_CREAT , S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+if (lockfd < 0) {
+	fprintf(stderr,"Error: lockfile %s could not be opened or created. Error %d(%s)\n",lockfile,errno,strerror(errno));
+	exit(-1);
+}; // end if
+
+// init vars for main loop
+laststate=-1;
+
+while (FOREVER) {
+
+	lret = lockf(lockfd, F_TEST, 0);
+
+	if (lret < 0) {
+		// file is locked,
+		global.out = 1;
+		newstate = 1;
+	} else {
+	// file is not locked
+		global.out = 0;
+		newstate = 0;
+	}; // end else - if
+
+	if (laststate != newstate) {
+		// wait for 1 second
+		printf("last state was %d, now %d\n",laststate,newstate);
+		laststate=newstate;
+		sleep(1);
+	}; // end if		
+
+	// wait 50 ms
+	usleep(50000);
+
+}; // end endless loop while
+
+fprintf(stderr,"Error: main application drops out of endless loop. Should not happen! \n");
+exit(-1);
+
+
+}; // end main application
+
+
+///////////////////////////////
 // function funct_writetochild
 // subthread
 
@@ -330,3 +321,58 @@ while (FOREVER) {
 fprintf(stderr,"Error: write-to-child thread dropped out of endless loop. Should not happen!\n");
 exit(-1);
 }; // end function writetochild
+
+
+///////////////////////////////
+// function do_child
+// subthread
+void * do_child (globaldatastr * p_global) {
+int ret;
+char c;
+
+
+// endless loop
+while (FOREVER) {
+
+
+	// first check: check parent process id
+
+	if (getppid() == 1) {
+		// if this is 1, this means that the parent process has died; so the program should terminate
+		fprintf(stderr,"Error: child process detects no more parent process: quiting! \n");
+		exit(-1);
+	}; // end if
+
+
+	// read data from PIPE
+	ret=read(p_global->readpipe,&c,1);
+
+	if (ret) {
+		if (c == 0x30) {
+			// Turn it off
+			bcm2835_gpio_write(PIN, LOW);
+
+		} else if (c == 0x31) {
+			// Turn it on
+			bcm2835_gpio_write(PIN, HIGH);
+
+		} else {
+			// error: unknown char
+			fprintf(stderr,"Warning: Child received unknown char from parent: %02X\n",c);
+		}; // end else - elsif - if
+	
+		// sleep 50 ms
+		usleep(50000);
+	} else {
+		// error in read
+		fprintf(stderr,"Error: Child did not receive any data from parent\n");
+
+		// sleep 1 second
+		sleep(1);
+	};  // read successfull
+
+
+}; // end while (endless loop)
+
+
+}; // end function do_child
